43e4dd08fea6e3508e26416551dde680f9fc80be
unknown <vmehr@naver.com>
2023-02-22 15:02:17 +0900

    문제풀이

---
+++ boj1003.cpp
@@ -0,0 +1,34 @@
+//boj1003번_피보나치 함수_dp
+
+#include<iostream>
+
+using namespace std;
+
+int dp[41];
+
+int main() {
+	int T; //테스트 케이스
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		dp[0] = 0;
+		dp[1] = 1;
+		dp[2] = 1;
+
+		for (int j = 3; j <= 40; j++) { //피보나치수를 dp를 이용해 구하기(시간 초과 방지)
+			dp[j] = dp[j - 1] + dp[j - 2];
+		}
+
+		int N;
+		cin >> N;
+
+		if (N == 0) {
+			cout << "1 0" << "\n";
+		}
+		else if (N == 1) {
+			cout << "0 1" << "\n";
+		}
+		else {
+			cout << dp[N - 1] << " " << dp[N] << "\n";
+		}
+	}
+}
