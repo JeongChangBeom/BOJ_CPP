fb03a5c76fc0eb9495670441bb341625236c4a4e
unknown <vmehr@naver.com>
2023-07-28 00:23:48 +0900

    문제풀이

---
+++ boj15650.cpp
@@ -0,0 +1,35 @@
+//boj15650번_N과 M (2)_백트래킹
+
+#include <iostream>
+
+using namespace std;
+
+int arr[9];
+bool visited[9];
+int N, M;
+
+void DFS(int x, int y) {
+	if (y == M) {
+		for (int i = 0; i < M; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << endl;
+		return;
+	}
+
+	for (int i = x; i <= N; i++) {
+		if (visited[i] == false) {
+			visited[i] = true;
+			arr[y] = i;
+			DFS(i + 1, y + 1);
+			visited[i] = false;
+		}
+	}
+}
+
+int main() {
+	cin >> N >> M;
+	DFS(1, 0);
+
+	return 0;
+}
