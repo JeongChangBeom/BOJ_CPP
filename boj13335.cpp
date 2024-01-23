384e5f20904db4f858292cc08896671747dd2190
unknown <vmehr@naver.com>
2024-01-23 15:52:07 +0900

    문제풀이

--- boj13335.cpp
+++ boj13335.cpp
@@ -2,8 +2,6 @@
 
 #include<iostream>
 #include<queue>
-#include<vector>
-#include<algorithm>
 
 using namespace std;
 
@@ -14,30 +12,37 @@ int main() {
 	vector<int> v;
 
 	for (int i = 0; i < n; i++) {
-		int num;
-		cin >> num;
-		v.push_back(num);
+		int car;
+		cin >> car;
+		v.push_back(car);
 	}
 
 	queue<int> q;
-
-	int sum = 0;
+	int weight = 0;
 	int result = 0;
 
 	for (int i = 0; i < n; i++) {
-		while (sum + v[i] > L) {
+		while (true) {
 			if (q.size() == w) {
-				sum -= q.front();
+				weight -= q.front();
 				q.pop();
 			}
+
+			if (v[i] + weight <= L) {
+				break;
+			}
+
 			q.push(0);
 			result++;
 		}
 		q.push(v[i]);
-		sum += v[i];
 		result++;
+		weight += v[i];
 	}
+
 	cout << result + w;
 }
-//큐를 이용해서 풀 수 있는 문제.
-//출력할 때 마지막 트럭이 건너가는 시간을 더해주는 것을 까먹고 실수 하지 말자.
+//자료구조 큐 문제.
+//입력 받은 트럭을 전부 vector에 저장해놨다가 queue로 하나씩 최대하중과 다리의 길이를 잘생각해서 옮기면 되는 문제이다.
+//핵심은 마지막 트럭이 혼자 지나 갈때 걸리는 시간인 다리길이(w)를 결과값(result)에 더해주는 것이다.
+//이전에 풀었었는데 백준에서 메모리 초과가 나서 다시 푼 문제이다. (풀었을 당시에는 통과가 됐었나보다...)
