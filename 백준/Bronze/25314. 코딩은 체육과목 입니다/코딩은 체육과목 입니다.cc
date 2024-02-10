6a5528e9364be5c2af61f472580dc3e5ec6b96e4
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-02-10 16:57:26 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,16 @@
+//boj25314번_코딩은 체육과목 입니다_구현
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N / 4; i++) {
+		cout << "long" << " ";
+	}
+
+	cout << "int";
+}
