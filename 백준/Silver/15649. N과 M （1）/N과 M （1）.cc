d41db0fb7822e15ec406aaf70de1a703255bc901
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-10-13 19:51:29 +0900

    [Silver III] Title: N과 M (1), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int arr[9];
+bool visited[9];
+int N, M;
+
+void DFS(int V) {
+	if (V == M) {
+		for (int i = 0; i < M; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 1; i <= N; i++) {
+		if (visited[i] == false) {
+			visited[i] = true;
+			arr[V] = i;
+			DFS(V + 1);
+			visited[i] = false;
+		}
+	}
+}
+
+int main() {
+	cin >> N >> M;
+	DFS(0);
+
+	return 0;
+}
