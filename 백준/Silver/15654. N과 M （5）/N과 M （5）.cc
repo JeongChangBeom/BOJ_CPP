53e18f7b999b2d90f48ab78b2c6938d79a740841
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-10-20 14:39:02 +0900

    [Silver III] Title: N과 M (5), Time: 32 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,45 @@
+//boj15654번_N과 M (5)_백트래킹
+
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+int arr[9];
+int result[9];
+
+bool visited[9];
+
+int N, M;
+
+void DFS(int v, int num) {
+	if (v == M) {
+		for (int i = 0; i < M; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 0; i < N; i++) {
+		if (!visited[i]) {
+			visited[i] = true;
+			arr[v] = result[i];
+			DFS(v + 1, i + 0);
+			visited[i] = false;
+		}
+	}
+}
+
+int main() {
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		cin >> result[i];
+	}
+
+	sort(result, result + N);
+
+	DFS(0, 1);
+}
